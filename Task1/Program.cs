dce5cad26571ff4355297ea461f7e7106a0495e1
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-24 16:51:12 +0300

    af\\Factorial

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+//Factorial(5);
+
+
+double Factorial(double n){
+    if(n == 1){
+        return 1;}
+
+    
+    return n * Factorial(n - 1); 
+    
+}Console.Write(Factorial(50));
+//Console.Write(Factorial(5));
