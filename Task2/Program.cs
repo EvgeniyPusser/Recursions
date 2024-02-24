d8633440fc4ad8bfc1db5f27adfcda9acf9ebe85
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-24 18:34:24 +0300

    FibbO

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿ void Fibbo(int a, int b){
+  //Console.Write($" {b}");
+    if(b > 30){
+        return;
+    }
+    b = a + b;
+   
+    a = b - a;
+ //Console.Write(a + "  ");
+    
+//  Console.Write(b + "  ");
+    // b = a + b;
+    //  Console.Write(a);
+    Console.Write($"{b}  ");пше фвв
+    // //Console.Write($"{a},\t {b}");
+    Fibbo(a , b);
+    
+  }
+   Console.Write($"1, 2");
+  Fibbo(1,2);
+ 
+    
+
+
+
