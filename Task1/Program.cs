8536ec3663ab049da2d361d35390a74d605a980e
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-26 19:57:08 +0300

    All push

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,14 +1,20 @@
-﻿
-
-//Factorial(5);
-
-
-double Factorial(double n){
-    if(n == 1){
-        return 1;}
+﻿// double Factorial(double n){
+//     if(n == 1){
+//         return 1;}
 
     
-    return n * Factorial(n - 1); 
+//     return n * Factorial(n - 1); 
     
-}Console.Write(Factorial(50));
+// }
+
 //Console.Write(Factorial(5));
+
+
+void OpenMatrioshka(int size){
+    if(size == 1){
+        return;
+    }
+    OpenMatrioshka(size - 1);
+    Console.WriteLine($"Open Matryioshla of size {size}...");
+   
+}OpenMatrioshka(7);
